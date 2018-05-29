0344f0c2b6120331026be5aca1c56a784ebd3ef9
GrantK <grantklees108@gmail.com>
2018-05-29 15:45:49 -0700

    Added Feature PrecisePercentage

--- Final.java
+++ Final.java
@@ -23,21 +23,31 @@ public class Final
         curGrade = reader.nextDouble();
         System.out.println("How much is your final worth? (80% = 80, 81.5% = 81.5)");
         finalWorth = reader.nextDouble();
-        double Grade00 = curGrade;
-        double Grade10 = curGrade + finalWorth*.1;
-        double Grade20 = curGrade + finalWorth*.2;
-        double Grade30 = curGrade + finalWorth*.3;
-        double Grade40 = curGrade + finalWorth*.4;
-        double Grade50 = curGrade + finalWorth*.5;
-        double Grade60 = curGrade + finalWorth*.6;
-        double Grade70 = curGrade + finalWorth*.7;
-        double Grade80 = curGrade + finalWorth*.8;
-        double Grade90 = curGrade + finalWorth*.9;
-        double Grade100 = curGrade + finalWorth*1;
-        double newTotal = 100 + finalWorth;
+        double currentGradeEffect = (100 - finalWorth)/100;
+        double newCurGrade = curGrade * currentGradeEffect;
         for(double i = 0.0; i <=1 ; i += 0.1)
         {
-            System.out.printf("Grade at a %.2f%% on the final: %f\n", i * 100.0, (curGrade + finalWorth*i)/newTotal);
+            System.out.printf("Grade at a %.2f%% on the final: %.2f%%\n", i * 100.0, (newCurGrade + finalWorth*i));
+        }
+        boolean precisePercent = true;
+        double checkPercentage = 0;
+        System.out.println("Would you like to check for a precise percentage? (Y/N)");
+        System.gc();
+        String preciseCheckAnswer = reader.next();
+        preciseCheckAnswer = preciseCheckAnswer.toLowerCase();
+        System.out.println(preciseCheckAnswer);
+        if(preciseCheckAnswer.equals("n"))
+            precisePercent = false;
+        while (precisePercent)
+        {
+            System.out.println("Please Enter The Perecent On the Final\nYou Would Like to Check Your Ending Grade For: \n(As whole number 80% = 80)");
+            checkPercentage = reader.nextDouble();
+            System.out.printf("Grade at a %.1f%% on the final: %.2f%%\n", checkPercentage, (newCurGrade + finalWorth*(checkPercentage/100)));
+            System.out.println("Would You Like to Check Another Percentage (Y/N)");
+            String againAnswer = reader.next();
+            againAnswer = againAnswer.toLowerCase();
+            if(againAnswer.equals("n"))
+                precisePercent = false;
         }
     }
 }
